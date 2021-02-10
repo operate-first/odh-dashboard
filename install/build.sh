56516d7971162f1d985521e173ea9f344b4dfa13
Tomas Coufal <tcoufal@redhat.com>
2021-02-10 12:41:31 +0100

    chore: Update build process for the OPF fork
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- install/build.sh
+++ install/build.sh
@@ -5,4 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Building ${IMAGE_REPOSITORY} from ${SOURCE_REPOSITORY_URL} on ${SOURCE_REPOSITORY_REF}"
 
-s2i build ${SOURCE_REPOSITORY_URL} --ref ${SOURCE_REPOSITORY_REF} --context-dir / registry.access.redhat.com/ubi8/nodejs-14 ${IMAGE_REPOSITORY}
+rm -rf .build
+mkdir .build
+
+s2i build ${SOURCE_REPOSITORY_URL} --ref ${SOURCE_REPOSITORY_REF} --context-dir / registry.access.redhat.com/ubi8/nodejs-14 ${IMAGE_REPOSITORY} --as-dockerfile .build/Dockerfile
+
+podman build .build -t ${IMAGE_REPOSITORY}
